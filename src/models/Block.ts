db21587c74f5e1ed862043bc0df20dc0db018050
nappingCommodore <niks.sbg@gmail.com>
2020-07-08 19:35:33 +0530

    Block structure push

---
+++ src/models/Block.ts
@@ -0,0 +1,20 @@
+export default class Block {
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public timestamp: number;
+    public data: string;
+
+    constructor(index: number, hash: string, previousHash: string, timestamp: number, data: string) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.timestamp = timestamp;
+        this.data = data;
+    }
+
+    calculateHash = (index: number, previousHash: number, timestamp: number, data: string) => {
+
+    }
+}
