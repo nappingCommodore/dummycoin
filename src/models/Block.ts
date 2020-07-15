98728b74d50c5dd8497baea8e2c661909d39c14f
nappingCommodore <niks.sbg@gmail.com>
2020-07-15 20:52:43 +0530

    Pushing Block structure

--- src/models/Block.ts
+++ src/models/Block.ts
@@ -1,10 +1,10 @@
 export default class Block {
 
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public timestamp: number;
-    public data: string;
+    private index: number;
+    private hash: string;
+    private previousHash: string;
+    private timestamp: number;
+    private data: string;
 
     constructor(index: number, hash: string, previousHash: string, timestamp: number, data: string) {
         this.index = index;
@@ -14,7 +14,27 @@ export default class Block {
         this.data = data;
     }
 
-    calculateHash = (index: number, previousHash: number, timestamp: number, data: string) => {
+    getIndex = (): number => {
+        return this.index;
+    }
 
+    getHash = (): string => {
+        return this.hash;
+    }
+
+    getPreviousHash = (): string => {
+        return this.previousHash;
+    }
+
+    getTimestamp = (): number => {
+        return this.timestamp;
+    }
+
+    getData = (): string => {
+        return this.data;
+    }
+
+    calculateHash = (index: number, previousHash: number, timestamp: number, data: string) => {
+        
     }
 }
