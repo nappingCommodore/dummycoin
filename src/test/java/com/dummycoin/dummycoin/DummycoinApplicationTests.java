e52ed0f0841af4a1713e045d935708df03990676
nappingCommodore <niks.sbg@gmail.com>
2020-08-22 22:02:04 +0530

    java code added

---
+++ src/test/java/com/dummycoin/dummycoin/DummycoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dummycoin.dummycoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DummycoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
